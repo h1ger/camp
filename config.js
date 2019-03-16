3cdf7e77e59e779a9ae437f85261f21e71bf22e5
Jisee Eun <jcrepogit@gmail.com>
2019-03-16 14:54:12 +0800

    env setting

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {}
+
+config.appconfig = {}
+
+// app config
+config.appconfig = process.env.DATABASEURL = "mongodb+srv://h1ger:Biancadenise8241@cluster0-5s46f.mongodb.net/test?retryWrites=true"
